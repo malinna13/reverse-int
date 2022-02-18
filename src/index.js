5c5904d4dd7938c618e95b07215c36da987c3d66
Inna Malinovskaya <malinna13@gmail.com>
2022-02-18 12:14:13 +0600

    feat: task done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let result = "";
+    let str = String(Math.abs(n));
+    for (let i = 0; i < str.length; i++) {
+        result = str[i] + result;
+    }
+    return result;
+};
